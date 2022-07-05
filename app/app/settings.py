75e0d53c8cabb06125f27acc3b740eec967a2398
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-07-05 12:55:38 +0300

    Finish user account!

--- app/app/settings.py
+++ app/app/settings.py
@@ -70,6 +70,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
